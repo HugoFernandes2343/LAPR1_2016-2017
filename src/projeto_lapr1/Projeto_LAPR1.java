5b44647ad1ac0590e8b9482975fcb43f8f663689
Marco Carneiro <1160777@isep.ipp.pt>
2016-12-16 10:32:30 +0000

    Implementação da lib (dúvidas tiradas com o stor)

--- src/projeto_lapr1/Projeto_LAPR1.java
+++ src/projeto_lapr1/Projeto_LAPR1.java
@@ -21,75 +21,97 @@ public class Projeto_LAPR1 {
 
     public static double[] RandomConsistency = {0, 0, 0.58, 0.90, 1.12, 1.24, 1.32, 1.42};/*Vetor Randômio até ao n=8*/
     public static int N_MATRIZES = 4;/*Nº de critérios + a matriz de comparação de critérios*/
+    public static String[] v_criterios = new String[4];
+    public static String[] v_criterio1 = new String[5];
+    public static String[] v_criterio2 = new String[5];
+    public static String[] v_criterio3 = new String[5];
+    public static double[][] mc_criterios = new double[3][3];
+    public static double[][] matrizCriterio1 = new double[4][4];
+    public static double[][] matrizCriterio2 = new double[4][4];
+    public static double[][] matrizCriterio3 = new double[4][4];
 
     public static void main(String[] args) throws FileNotFoundException {
         int nLinhas = 0;
-        String[] v_criterios=new String [4];String[] v_criterio1=new String [5];
-        String[] v_criterio2=new String [5];String[] v_criterio3=new String [5];
-        double[][] mc_criterios = new double[3][3];double[][] matrizCriterio1 = new double[4][4];
-        double[][] matrizCriterio2 = new double[4][4];double[][] matrizCriterio3 = new double[4][4];
-        nLinhas = LerFicheiroInput(nLinhas,v_criterios,mc_criterios,v_criterio1,matrizCriterio1,v_criterio2,matrizCriterio2,v_criterio3,matrizCriterio3);
-        
+
+        nLinhas = LerFicheiroInput(nLinhas, v_criterios, mc_criterios, v_criterio1, matrizCriterio1, v_criterio2, matrizCriterio2, v_criterio3, matrizCriterio3);
+
+        mc_criterios = normalizar(mc_criterios);
+
         Matrix criterios = new Basic2DMatrix(mc_criterios);
         Matrix criterio1 = new Basic2DMatrix(matrizCriterio1);
         Matrix criterio2 = new Basic2DMatrix(matrizCriterio2);
         Matrix criterio3 = new Basic2DMatrix(matrizCriterio3);
-        
-        FazerVetoresProprios(criterios,criterio1,criterio2,criterio3);
+        FazerVetoresProprios(criterios, criterio1, criterio2, criterio3);
     }
 
-    public static int LerFicheiroInput(int nLinhas,String[] v_criterios,double[][] mc_criterios,String[] v_criterio1,double[][] matrizCriterio1,String[] v_criterio2,double[][] matrizCriterio2,String[] v_criterio3,double[][] matrizCriterio3) throws FileNotFoundException {
+    public static int LerFicheiroInput(int nLinhas, String[] v_criterios, double[][] mc_criterios, String[] v_criterio1, double[][] matrizCriterio1, String[] v_criterio2, double[][] matrizCriterio2, String[] v_criterio3, double[][] matrizCriterio3) throws FileNotFoundException {
 
         /*Formatter ler = new Formatter(new File("Dados.txt"));*/
         Scanner readFile = new Scanner(new File("Dados.txt"));
         while (readFile.hasNext()) {
             String linhaDados = readFile.nextLine();
             if (linhaDados.length() > 0) {
-                nLinhas = tratarInput(linhaDados,v_criterios,mc_criterios,v_criterio1, matrizCriterio1,v_criterio2, matrizCriterio2,v_criterio3, matrizCriterio3, nLinhas);
+                nLinhas = tratarInput(linhaDados, v_criterios, mc_criterios, v_criterio1, matrizCriterio1, v_criterio2, matrizCriterio2, v_criterio3, matrizCriterio3, nLinhas);
             }
         }
         readFile.close();
         return nLinhas;
     }
 
-    public static int tratarInput(String linhaDados, String[] v_criterios,double[][] mc_criterios,String[] v_criterio1, double[][] matrizCriterio1,String[] v_criterio2, double[][] matrizCriterio2,String[] v_criterio3, double[][] matrizCriterio3, int nLinhas) {
+    public static int tratarInput(String linhaDados, String[] v_criterios, double[][] mc_criterios, String[] v_criterio1, double[][] matrizCriterio1, String[] v_criterio2, double[][] matrizCriterio2, String[] v_criterio3, double[][] matrizCriterio3, int nLinhas) {
         String temp[] = linhaDados.split(" ");/*Split testes*/
-        /*String nomeMatriz = temp[0];*/
+ /*String nomeMatriz = temp[0];*/
         for (int j = 0; j < temp.length; j++) {
-            if (temp.length == 4 && nLinhas == 0) {
+            if (nLinhas == 0) {
                 v_criterios[j] = temp[j];
-            } else if (temp.length == 3 && nLinhas > 0 && nLinhas < 5) {
-                mc_criterios[nLinhas-1][j] = StringToDouble(temp,j);
-            } else if (temp.length == 5 && nLinhas == 4) {
+            } else if (nLinhas > 0 && nLinhas < 4) {
+                mc_criterios[nLinhas - 1][j] = StringToDouble(temp, j);
+            } else if (nLinhas == 4) {
                 v_criterio1[j] = temp[j];
-            } else if (temp.length == 4 && nLinhas > 4 && nLinhas < 9) {
-                matrizCriterio1[nLinhas - mc_criterios.length-2/*Linhas ocupadas pelos vetores*/][j] = StringToDouble(temp,j);
-            } else if (temp.length == 5 && nLinhas == 9) {
+            } else if (nLinhas > 4 && nLinhas < 9) {
+                matrizCriterio1[nLinhas - mc_criterios.length - 2/*Linhas ocupadas pelos vetores*/][j] = StringToDouble(temp, j);
+            } else if (nLinhas == 9) {
                 v_criterio2[j] = temp[j];
-            } else if (temp.length == 4 && nLinhas > 9 && nLinhas < 14) {
-                matrizCriterio2[nLinhas - mc_criterios.length - matrizCriterio1.length-3][j] = StringToDouble(temp,j);
-            } else if (temp.length == 5 && nLinhas == 14) {
+            } else if (nLinhas > 9 && nLinhas < 14) {
+                matrizCriterio2[nLinhas - mc_criterios.length - matrizCriterio1.length - 3][j] = StringToDouble(temp, j);
+            } else if (nLinhas == 14) {
                 v_criterio3[j] = temp[j];
-            } else if (temp.length == 4 && nLinhas > 14) {
-                matrizCriterio3[nLinhas - mc_criterios.length - matrizCriterio1.length - matrizCriterio3.length-4][j] = StringToDouble(temp,j);
+            } else if (nLinhas > 14) {
+                matrizCriterio3[nLinhas - mc_criterios.length - matrizCriterio1.length - matrizCriterio3.length - 4][j] = StringToDouble(temp, j);
             }
         }
         nLinhas++;
         return nLinhas;
     }
-    
-    public static Double StringToDouble(String[] temp,int j){
-        if(temp[j].contains("/")){
+
+    public static double StringToDouble(String[] temp, int j) {
+        if (temp[j].contains("/")) {
             String[] tempDiv = temp[j].split("/");
-            return Double.parseDouble(tempDiv[0])/Double.parseDouble(tempDiv[1]);
-        }else{
+            return Double.parseDouble(tempDiv[0]) / Double.parseDouble(tempDiv[1]);
+        } else {
             return Double.parseDouble(temp[j]);
         }
     }
-    
-    public static void FazerVetoresProprios(Matrix criterios,Matrix criterio1,Matrix criterio2,Matrix criterio3){
-        //pedir vetores proprios e valores
+
+    public static double[][] normalizar(double[][] matriz) {
+        
+        return matriz;
+    }
+
+    public static void FazerVetoresProprios(Matrix criterios, Matrix criterio1, Matrix criterio2, Matrix criterio3) {
+        //Pedir vetores proprios e valores
         EigenDecompositor eigenD = new EigenDecompositor(criterios);
-        Matrix[] criteriosD=eigenD.decompose();
+        Matrix[] criteriosD = eigenD.decompose();
+        for (int i = 0; i < criteriosD.length; i++) {
+            System.out.println(criteriosD[i]);
+        }
+        //Converter objeto Matrix (que sao duas matrizes) para array
+        /*double matA[][] = criteriosD[0].toDenseMatrix().toArray();
+        double matB[][] = criteriosD[1].toDenseMatrix().toArray();
+        for(int i =0;i<matA.length;i++){
+            for(int j=0;j<matB.length;j++){
+                System.out.println(matA[i][j]);
+            }
+        }*/
     }
 }
