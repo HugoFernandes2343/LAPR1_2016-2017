8a4631f931c928ad2dc2846c35c8f921fb7a77b7
Hugo Carvalho <1161569@isep.ipp.pt>
2017-01-09 19:10:11 +0000

    Já tem os metodos que dão origem á matriz normalizada.

--- src/projetolapr1_iteracao2/MetodoTOPSIS.java
+++ src/projetolapr1_iteracao2/MetodoTOPSIS.java
@@ -3,7 +3,8 @@ package projetolapr1_iteracao2;
 import java.io.File;
 import java.util.Scanner;
 import java.util.Formatter;
-import java.io.FileNotFoundException;/*
+import java.io.FileNotFoundException;
+import java.lang.Math;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -18,6 +19,7 @@ public class MetodoTOPSIS {
     public static void main(String[] args) throws FileNotFoundException {
         double[] pesos;
         double[][] MC;
+        double[][] MNorm;
         String[][] totalInput = new String[50][50];
         String nomeFich = "inputTOPSIS.txt";
         String[] beneficios;
@@ -37,7 +39,7 @@ public class MetodoTOPSIS {
         nElementos = encontrarNEelementos(totalInput[6]);
         alternativas = comporArray(totalInput[6], nElementos);
         MC = criarMatrizCriterios(totalInput, alternativas, criterios);
-
+        MNorm = matrizNormalizada(MC, alternativas, criterios);
     }
 
     public static int LerFicheiroInput(String Input, String[][] totalInput, int nLinhas) throws FileNotFoundException {
@@ -98,4 +100,31 @@ public class MetodoTOPSIS {
         return MC;
     }
 
+    public static double[][] matrizNormalizada(double[][] MC, String[] alternativas, String[] criterios) {
+        double temp[][] = new double[criterios.length][alternativas.length];
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[1].length; j++) {
+                temp[i][j] = Math.pow(MC[i][j], 2);
+            }
+        }
+
+        return operaracaoSomatorio(temp);
+    }
+
+    public static double[][] operaracaoSomatorio(double[][] temp) {
+        double soma = 0;
+        double[] somatorio = new double[temp[1].length];
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[1].length; j++) {
+                soma = soma + temp[i][j];
+            }
+            somatorio[i] = soma;
+        }
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[1].length; j++) {
+                temp[i][j]=temp[i][j]/somatorio[i];
+            }
+        }
+        return temp;
+    }
 }
