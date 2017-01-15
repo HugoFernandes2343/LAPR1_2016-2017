6bb82ee58ba520da9d954ac36cc1d5c30baf8c1f
Hugo Carvalho <1161569@isep.ipp.pt>
2017-01-15 18:08:23 +0000

    Conclusão dos testes unitários.

--- src/PackageTestes/Testes.java
+++ src/PackageTestes/Testes.java
@@ -26,7 +26,6 @@ public class Testes {
         somatoriosColunas_teste();
         matrizNormalizada_teste();
         System.out.println("");
-        operacaoSomatorio_teste();
         System.out.println("");
         matrizPesada_teste();
         System.out.println("");
@@ -135,31 +134,31 @@ public class Testes {
 
     /*normalizar a matriz inicial*/
     public static void matrizNormalizada_teste() {
-        double[][] matriznormal = {{5, 6}, {7, 4}};
-        double[][] matriznormalizadaesperada = {{0.58123, 0.83205}, {0.81373, 0.55470}};
+        double[][] matrizNormal = {{5, 6}, {7, 4}};
+        double[][] matrizNormalizadaEsperada = {{0.58123, 0.83205}, {0.81373, 0.55470}};
         String[] crit = {"custo", "estilo"};
         String[] alt = {"marca1", "marca2"};
 
         System.out.println("");
         System.out.println("Matriz:");
 
-        for (int i = 0; i < matriznormal.length; i++) {
-            for (int j = 0; j < matriznormal[i].length; j++) {
-                System.out.printf("%20s", matriznormal[i][j]);
+        for (int i = 0; i < matrizNormal.length; i++) {
+            for (int j = 0; j < matrizNormal[i].length; j++) {
+                System.out.printf("%20s", matrizNormal[i][j]);
             }
             System.out.println("");
         }
         System.out.println("");
         System.out.println("Matriz Esperada:");
 
-        for (int i = 0; i < matriznormalizadaesperada.length; i++) {
-            for (int j = 0; j < matriznormalizadaesperada[i].length; j++) {
-                System.out.printf("%20s", matriznormalizadaesperada[i][j]);
+        for (int i = 0; i < matrizNormalizadaEsperada.length; i++) {
+            for (int j = 0; j < matrizNormalizadaEsperada[i].length; j++) {
+                System.out.printf("%20s", matrizNormalizadaEsperada[i][j]);
             }
             System.out.println("");
         }
         System.out.println("Output");
-        double[][] matriznormalizada = MetodoTOPSIS.matrizNormalizada(matriznormal, alt, crit);
+        double[][] matriznormalizada = MetodoTOPSIS.matrizNormalizada(matrizNormal, alt, crit);
         for (int i = 0; i < matriznormalizada.length; i++) {
             for (int j = 0; j < matriznormalizada[i].length; j++) {
                 System.out.printf("%20s", matriznormalizada[i][j]);
@@ -168,23 +167,112 @@ public class Testes {
         }
     }
 
-    /*efetuar o somatorio das colunas na matriz normalizada*/
-    public static void operacaoSomatorio_teste() {
-
-    }
 
     /* depois de efetuar o somatorio multiplica-se a cada coluna pelo respetivo peso do criterio*/
     public static void matrizPesada_teste() {
+        double[][] matrizNormalizada = {{0.58123, 0.83205}, {0.81373, 0.55470}};
+        double[] vecPesos = {0.4, 0.6};
+        double[][] matrizNormalizadaPesadaEsperada = {{0.232492, 0.49923}, {0.325492, 0.33282}};
 
+        System.out.println("");
+        System.out.println("Vetor pesos:");
+        for (int i = 0; i < vecPesos.length; i++) {
+            System.out.printf("%20s", vecPesos[i]);
+            System.out.println("");
+        }
+
+        System.out.println("");
+        System.out.println("Matriz:");
+        for (int i = 0; i < matrizNormalizada.length; i++) {
+            for (int j = 0; j < matrizNormalizada[i].length; j++) {
+                System.out.printf("%20s", matrizNormalizada[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("Matriz Esperada:");
+
+        for (int i = 0; i < matrizNormalizadaPesadaEsperada.length; i++) {
+            for (int j = 0; j < matrizNormalizadaPesadaEsperada[i].length; j++) {
+                System.out.printf("%20s", matrizNormalizadaPesadaEsperada[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("Output");
+        double[][] matrizNormalizadaPesada = MetodoTOPSIS.matrizPesada(matrizNormalizada, vecPesos);
+        for (int i = 0; i < matrizNormalizadaPesada.length; i++) {
+            for (int j = 0; j < matrizNormalizadaPesada[i].length; j++) {
+                System.out.printf("%20s", matrizNormalizadaPesada[i][j]);
+            }
+            System.out.println("");
+        }
     }
 
     /* utilizar a matriz pesada e efetuar a raiz do somatorio de cada linha para os valores ideais positivos*/
     public static void detDistanciaIdealP_teste() {
+        double[][] matrizNormalizadaPesada = {{0.232492, 0.49923}, {0.325492, 0.33282}};
+        double[][] matrizValoresIdeais = {{0.325492, 0.49923}, {0.232492, 0.33282}};
+        String[] crit = {"custo", "estilo"};
+        String[] alt = {"marca1", "marca2"};
+        double[] vetorDistanciaIdealEsperado = {0.093, 0.166};
+        System.out.println("");
+        System.out.println("Matriz Pesada:");
+        for (int i = 0; i < matrizNormalizadaPesada.length; i++) {
+            for (int j = 0; j < matrizNormalizadaPesada[i].length; j++) {
+                System.out.printf("%20s", matrizNormalizadaPesada[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("Valores ideais:");
+        for (int i = 0; i < matrizNormalizadaPesada.length; i++) {
+            System.out.printf("%20s", matrizValoresIdeais[0][i]);
+        }
 
+        System.out.println("");
+        System.out.println("Distancia esperada:");
+        for (int i = 0; i < vetorDistanciaIdealEsperado.length; i++) {
+            System.out.printf("%20s", vetorDistanciaIdealEsperado[i]);
+        }
+        System.out.println("");
+        System.out.println("Output");
+        double[] vetorDistanciaIdeal = MetodoTOPSIS.detDistanciaIdealP(matrizNormalizadaPesada, alt, crit, matrizValoresIdeais);
+        for (int i = 0; i < vetorDistanciaIdeal.length; i++) {
+            System.out.printf("%20s", vetorDistanciaIdeal[i]);
+        }
     }
 
     /* mesmo metodo que foi acima enunciado mas para os valores negativos*/
     public static void detDistanciaIdealN_teste() {
+        double[][] matrizNormalizadaPesada = {{0.232492, 0.49923}, {0.325492, 0.33282}};
+        double[][] matrizValoresIdeais = {{0.325492, 0.49923}, {0.232492, 0.33282}};
+        String[] crit = {"custo", "estilo"};
+        String[] alt = {"marca1", "marca2"};
+        double[] vetorDistanciaIdealEsperado = {0.166, 0.093};
+        System.out.println("");
+        System.out.println("Matriz Pesada:");
+        for (int i = 0; i < matrizNormalizadaPesada.length; i++) {
+            for (int j = 0; j < matrizNormalizadaPesada[i].length; j++) {
+                System.out.printf("%20s", matrizNormalizadaPesada[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("Valores ideais negativos:");
+        for (int i = 0; i < matrizNormalizadaPesada.length; i++) {
+            System.out.printf("%20s", matrizValoresIdeais[1][i]);
+        }
 
+        System.out.println("");
+        System.out.println("Distancia esperada:");
+        for (int i = 0; i < vetorDistanciaIdealEsperado.length; i++) {
+            System.out.printf("%20s", vetorDistanciaIdealEsperado[i]);
+        }
+        System.out.println("");
+        System.out.println("Output");
+        double[] vetorDistanciaIdealN = MetodoTOPSIS.detDistanciaIdealN(matrizNormalizadaPesada, alt, crit, matrizValoresIdeais);
+        for (int i = 0; i < vetorDistanciaIdealN.length; i++) {
+            System.out.printf("%20s", vetorDistanciaIdealN[i]);
+        }
     }
 }
